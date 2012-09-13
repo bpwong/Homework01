e01c4044c3489bf4ac92e7d0d7b798cc1bb432c0
bpwong <bpwong@berkeley.edu>
2012-09-12 18:48:43 -0700

    added __name__ == "__main__" and calculator()

--- CalCalc.py
+++ CalCalc.py
@@ -1,4 +1,15 @@
 #/usr/bin/env python
-
-
 print 'hello github'
+   
+
+def calculator(str0 = '"Enter a string to calculate"'):
+    print eval(str0)
+
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)>1:
+        calculator((sys.argv[1]))
+    else:
+        calculator()
