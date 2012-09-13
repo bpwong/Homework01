70cc47c434137278130462254ea3dbf4b506aa60
bpwong <bpwong@berkeley.edu>
2012-09-12 23:33:35 -0700

    module + standalone attempt

--- CalCalc.py
+++ CalCalc.py
@@ -1,15 +1,68 @@
 #/usr/bin/env python
-print 'hello github'
+
+#print 'hello github'
+import argparse, sys
+'''
+    DO SHiT
+'''
+
+
+def calculator(args):
+    if args.print_switch:
+    #print eval(args.str0)
+        print 'asdfads'
+        if args.str0:
+            print args.str0
+        else:
+            print 'baha'
+#return eval(args.str0)
+
+if len(sys.argv)==2:
+    print sys.argv[:]
+
+parser = argparse.ArgumentParser(description='CalCalc Parser')
    
+parser.add_argument('-p', action='store_true', default=True,
+                    dest='print_switch',
+                    help='Print the output to the screen')
 
-def calculator(str0 = '"Enter a string to calculate"'):
-    print eval(str0)
+parser.add_argument('-s', action='store', dest='str0',
+                    help='Specify the expression to evaluate')
 
+    #parser.add_argument('-s', action='store', dest='str0',
+    #                help='Specify the expression to evaluate',
+#                default='(Enter a String)')
+
+
+inputs = parser.parse_args()
+
+calculator(inputs)
+
+
+
+
+
+def test_0():
+    assert abs(4. - calculator('2**2')) < .001
+
+def test_1():
+    assert ([0,0,0,1,2,3] == \
+            calculator('[0,0,0]+[1,2,3]')) == True
+
+def test_2():
+    assert abs(4. - calculator('2**2')) < .001
+
+def test_3():
+    assert abs(4. - calculator('2**2')) < .001
+
+def test_4():
+    assert abs(4. - calculator('2**2')) < .001
 
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv)>1:
-        calculator((sys.argv[1]))
-    else:
-        calculator()
+    print 'main'
+        #if len(sys.argv)>1:
+    #calculator((sys.argv[1]))
+        #else:
+#calculator(inputs)
