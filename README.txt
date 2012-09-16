620744ec39f14c76d9e892a8e32438f78c1e5872
bpwong <bpwong@berkeley.edu>
2012-09-16 01:24:10 -0700

    package ready

---
+++ README.txt
@@ -0,0 +1,29 @@
+=============
+UC Calculator
+=============
+
+UC Calculator (CalCalc) provides a calculator that returns the 
+value of any expression or query written given as a string. CalCalc
+can interface with Wolfram|Alpha for more complex questions.  
+
+CalCalc can be used from either the command line or imported from
+within Python.  You might find it most useful for tasks involving 
+simple arithmetic and also looking up constants (i.e. mass of the 
+earth).  
+
+Typical usage often looks like this::
+
+    $ python CalCalc.py -s '34*28'
+    $  952
+
+    $ python CalCalc.py 'mass of the moon in kg'
+    $  7.3459e+22
+
+    ---- AND, from within Python ----
+    >>> from CalCalc import calculate
+    >>> calculate('34*20')
+    >>> 952
+
+    >>> from CalCalc import calculate
+    >>> calculate('mass of the moon in kg',  return_float=True) * 10
+    >>> 7.3459e+23
---
+++ calculate/CalCalc.py
@@ -0,0 +1,116 @@
+import argparse, sys, urllib, urllib2
+from xml.etree import ElementTree as etree
+'''
+    DO SHiT
+'''
+
+# Credit to http://advencode.wordpress.com/2011/10/17/simple-query-request-with-wolfram-api/
+
+def _get_xml(ip):
+
+    appid = 'UAGAWR-3X6Y8W777Q'
+    base_url = 'http://api.wolframalpha.com/v2/query?'
+    headers = {'User-Agent':None}
+    
+    url_params = {'input':ip, 'appid':appid}
+    data = urllib.urlencode(url_params)
+    req = urllib2.Request(base_url, data, headers)
+    xml = urllib2.urlopen(req).read()
+    return xml
+
+        
+def _xmlparser(xml):
+    data_dics = {}
+    tree = etree.fromstring(xml)
+    #retrieving every tag with label 'plaintext'
+    for e in tree.findall('pod'):
+        for item in [ef for ef in list(e) if ef.tag=='subpod']:
+            for it in [i for i in list(item) if i.tag=='plaintext']:
+                if it.tag=='plaintext':
+                    data_dics[e.get('title')] = it.text
+    return data_dics
+        
+
+def calculate(in0, return_float = False):
+    try:
+        if return_float:
+            return float(eval(in0))        
+        return eval(in0)
+
+    except:
+        print 'Searching W|A...'
+        
+        xml = _get_xml(in0)
+        result_dics = _xmlparser(xml)
+        
+        try:
+            
+        
+            s = result_dics['Result']
+        
+            if not return_float:
+                return s
+        
+            # Remove units by searching for first space
+            s=s[:s.find(' ')]
+
+            # Check for scientific notation
+            if not s.find('10^') == -1:
+                s = s[:s.find('10^')-1] + 'e' + \
+                    s[s.find('^')+1:]
+
+            return float(s)
+        except:
+            return 'Try rephrasing your question'
+
+
+
+def test_0():
+    assert abs(4. - calculate('2**2')) < .001
+
+def test_1():
+    assert ([0,0,0,1,2,3] == \
+            calculate('[0,0,0]+[1,2,3]')) == True
+
+def test_2():
+    assert abs(4. - calculate('2**2')) < .001
+
+def test_3():
+    assert abs(4. - calculate('2**2')) < .001
+
+def test_4():
+    assert abs(4. - calculate('2**2')) < .001
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='CalCalc Parser')
+
+    parser.add_argument('tagless', nargs='?')
+    
+    
+    parser.add_argument('-s', action='store', dest='str0',
+                        help='Specify the expression to evaluate')
+
+    
+    parser.add_argument('-fl', action='store_true', default=False,
+                        dest='return_float',
+                        help='Return the value as a float')
+    comm = '''
+            parser.add_argument('-p', action='store_true', default=False,
+            dest='print_switch',
+            help='Print the output to the screen')
+           '''
+
+
+    args = parser.parse_args()
+    
+    # Has user indicated -s?
+    if args.str0:
+        out = calculate(args.str0, args.return_float)
+    else:
+        #print 'tagless'
+        out = calculate(args.tagless, args.return_float)
+    
+    print out
+
